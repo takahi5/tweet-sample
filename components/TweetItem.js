6f57ecbe95f1c7094e09cd92627305ffcd1d6aff
Takahiko Wada <wada.takahiko@gmail.com>
2019-08-24 15:33:50 +0900

    first commit

---
+++ components/TweetItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class TweetItem extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>TweetItem Component</Text>
+      </View>
+    );
+  }
+}
