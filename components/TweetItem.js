bfc111841d697b7c8b8b686a9b29bace9c2ec1fe
Takahiko Wada <wada.takahiko@gmail.com>
2019-08-30 10:39:39 +0900

    TweetItem styling

--- components/TweetItem.js
+++ components/TweetItem.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class TweetItem extends React.Component {
   render() {
     return (
-      <View>
-        <Text>TweetItem Component</Text>
+      <View style={styles.container}>
+        <View>
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={{
+              uri: 'https://facebook.github.io/react-native/img/tiny_logo.png',
+            }}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.nameText}>@Takeshi</Text>
+          <Text>Hello world</Text>
+          <Text>♡10</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    width: '100%',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 10,
+    borderColor: 'gray',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  textContainer: {
+    marginLeft: 10,
+    justifyContent: 'space-between',
+    height: '100%',
+  },
+  nameText: {
+    fontWeight: 'bold',
+  },
+});
