55d20253c46d067685fa70bb4b832c998b12729a
Takahiko Wada <wada.takahiko@gmail.com>
2019-08-30 13:36:02 +0900

    Use state for tweets

--- App.js
+++ App.js
@@ -3,22 +3,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import TweetItem from './components/TweetItem';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  this.state = {
+    tweets: [
+      {
+        name: 'Ken',
+        text: 'Hello world',
+        like: 15,
+        imageUri: 'https://picsum.photos/id/3/200/200',
+      },
+      {
+        name: 'Takeshi',
+        text: 'Hey',
+        like: 1,
+        imageUri: 'https://picsum.photos/id/1/200/200',
+      },
+    ],
+  };
+
+  const tweetItems = this.state.tweets.map((tweet, index) => {
+    return (
       <TweetItem
-        name="Ken"
-        text="Hello world"
-        like={15}
-        imageUri="https://picsum.photos/id/3/200/200"
+        name={tweet.name}
+        text={tweet.text}
+        like={tweet.like}
+        imageUri={tweet.imageUri}
+        key={index}
       />
-      <TweetItem
-        name="Taro"
-        text="Hey"
-        like={20}
-        imageUri="https://picsum.photos/id/1/200/200"
-      />
-    </View>
-  );
+    );
+  });
+  return <View style={styles.container}>{tweetItems}</View>;
 }
 
 const styles = StyleSheet.create({
